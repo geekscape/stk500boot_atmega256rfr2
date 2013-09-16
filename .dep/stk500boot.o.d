a7783cea638475930d50ec31897a042177062d2a
rdpowers <rdpowers@gmail.com>
2013-09-16 10:32:12 +1000

    Commit v1.1.

---
+++ .dep/stk500boot.o.d
@@ -0,0 +1,69 @@
+stk500boot.o: stk500boot.c \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include/stdint.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/sfr_defs.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iom256rfr2.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/boot.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/eeprom.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include/stddef.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include-fixed/limits.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/pgmspace.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/math.h \
+ /home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/stdlib.h \
+ command.h avr_cpunames.h
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include/stdint.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/sfr_defs.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iom256rfr2.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/boot.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/eeprom.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include/stddef.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/include-fixed/limits.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/avr/pgmspace.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/math.h:
+
+/home/netbook/avr8-gnu-toolchain-linux_x86/bin/../lib/gcc/avr/4.7.2/../../../../avr/include/stdlib.h:
+
+command.h:
+
+avr_cpunames.h:
